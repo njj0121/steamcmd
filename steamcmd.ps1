c0bd235d5cbe44fd7e3a8874c105474fd14f957e
njj0121 <94025771+njj0121@users.noreply.github.com>
2023-02-14 10:25:44 +0800

    Delete steamcmd.ps1

--- steamcmd.ps1
+++
@@ -1,6 +0,0 @@
-md c:\steamcmd
-cd c:\steamcmd
-curl -o steamcmd.zip https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip
-Expand-Archive -Path steamcmd.zip -DestinationPath .\
-del steamcmd.zip
-.\steamcmd.exe +quit
